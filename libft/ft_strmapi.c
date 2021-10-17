52e81d1c6c2d12df7bf6bb7b864079e1e152ccca
0x1nf3ct3d <leetpwnsroot@gmail.com>
2021-10-17 22:00:35 +0200

    N00

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,21 @@
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*ptr;
+	int	i;
+
+	i = 0;
+	if (!s || !f)
+		return (NULL);
+	ptr = malloc((ft_strlen(s) + 1) * sizeof(char));
+	if (!ptr)
+		return (NULL);
+	while (s[i])
+	{
+		ptr[i] = f(i, s[i]);
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
